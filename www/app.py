838795dab0a13a4945c75cfcb5dbcc7dafc4422e
bainuicf <bainui@msn.com>
2017-04-07 16:09:10 +0800

    20170407  完成orm.py，未测试！

--- www/app.py
+++ www/app.py
@@ -16,11 +16,15 @@ def index(request):
 @asyncio.coroutine
 def init(loop):
     app= web.Application(loop=loop)
-    app.router.add_route('GET','/',index)
-    srv= yield from loop.create_server(app.make_handler(),'127.0.0.1','9000')
+    app.router.add_route('GET',
+                         '/',
+                         index)
+    srv= yield from loop.create_server(app.make_handler(),
+                                       '127.0.0.1',
+                                       '9000')
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
 loop= asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
-loop.run_forever() 
+loop.run_forever()
